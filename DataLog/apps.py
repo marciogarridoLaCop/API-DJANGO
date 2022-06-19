5840467893e83120d0a9551c50faf62eea3bbb29
Marcio <marciogarrido@id.uff.br>
2022-06-19 15:20:03 -0300

    Tipo, Sensor e datalog

--- DataLog/apps.py
+++ DataLog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EspConfig(AppConfig):
+class DatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ESP'
+    name = 'DataLog'
