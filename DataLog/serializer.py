5cb0016268fc4531d8d36375535732a6a352481c
Marcio <marciogarrido@id.uff.br>
2022-06-19 16:52:21 -0300

    API DataLog OK

--- DataLog/serializer.py
+++ DataLog/serializer.py
@@ -1,3 +1,7 @@
 from rest_framework import serializers
 from DataLog.models import Registro
 
+class RegistroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Registro
+        fields = ['id', 'sensor', 'temperatura', 'umidade', 'pressao']
--- DataLog/views.py
+++ DataLog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 from DataLog.models import Registro
-# Create your views here.
+from DataLog.serializer import RegistroSerializer
+
+
+class RegistroViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os registros"""
+    queryset = Registro.objects.all()
+    serializer_class =  RegistroSerializer
