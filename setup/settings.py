758d52082535049c9e05e184dc4c71680ef76c4b
Marcio <marciogarrido@id.uff.br>
2022-10-22 18:56:42 -0300

    Banco PostgreSQL conectado

--- setup/settings.py
+++ setup/settings.py
@@ -78,12 +78,20 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mtljbq_esp',
+        'USER': 'mtljbq_esp',
+        'PASSWORD': '@OVO_ovo',
+        'HOST': 'postgres-ag-br1-3.conteige.cloud',
+        'PORT':'54149'
     }
 }
+
+
+
 
 
 # Password validation
