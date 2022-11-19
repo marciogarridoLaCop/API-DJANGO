e3c08a899b22f9fefabaf99a6b84d76a9b00ca2a
Marcio <marciogarrido@id.uff.br>
2022-11-18 21:30:23 -0300

    campo precipitacao criado

--- DataLogSensor/admin.py
+++ DataLogSensor/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from DataLogSensor.models import Registro
 
 class Dados(admin.ModelAdmin):
-    list_display = ('id','sensor','temperatura','pressao','altitude','pressa_nivel_mar','altitude_real','data_registro')
+    list_display = ('id','sensor','temperatura','pressao','precipitacao','altitude','pressa_nivel_mar','altitude_real','data_registro')
     list_display_links = ('id', 'sensor')
     search_fields = ('sensor',)
     list_per_page = 20
