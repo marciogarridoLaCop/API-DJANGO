bff791478616966830633a803681c7dddff321b8
Marcio <marciogarrido@id.uff.br>
2023-04-29 17:48:36 -0300

    heroku 2.0 nova AWS

--- setup/settings.py
+++ setup/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 
 import os
 
-import django_heroku
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,5 +131,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ## Número de registros exibidos pela API 
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
-
-django_heroku.settings(locals())
